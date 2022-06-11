84e357da97cbea745a20543385be156b4c88a882
Adohk <bloodmessenger@gmail.com>
2022-06-11 04:46:59 -0400

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,87 @@
+var words = "ADDED TO ALL MINUS NONE SHARED BY EVERYTHING MULTIPLIED IN ABUNDANCE"
+
+var alphabetMap = Array.from("abcdefghijklmnopqrstuvwxyz")
+function getCharGematria(c){
+	c = c.toLowerCase()
+	return alphabetMap.indexOf(c) + 1
+}
+
+function getWordsGematria(words){
+	var split = words.split(" ")
+	var resultArray = []
+	split.forEach(function(word){
+		var wordArray = Array.from(word)
+		var wordResult = 0
+		wordArray.forEach(function(character){
+			wordResult += getCharGematria(character)
+		})
+		resultArray.push(wordResult)
+	})
+	return resultArray
+}
+
+var pythagoraMap = {
+	"1": "ajs",
+	"2": "bkt",
+	"3": "clu",
+	"4": "dmv",
+	"5": "enw",
+	"6": "fox",
+	"7": "gpy",
+	"8": "hqz",
+	"9": "ir"
+}
+
+function getCharPythagora(c){
+	c = c.toLowerCase()
+	var result = 0
+	Object.values(pythagoraMap).forEach(function (letters){
+		if(letters.includes(c)){
+			result = parseInt(Object.keys(pythagoraMap)[Object.values(pythagoraMap).indexOf(letters)])
+		}		
+	})
+	return result
+}
+
+function getWordsPythagora(word){
+	var split = words.split(" ")
+	var resultArray = []
+	split.forEach(function(word){
+		var wordArray = Array.from(word)
+		var wordResult = 0
+		wordArray.forEach(function(character){
+			wordResult += getCharPythagora(character)
+		})
+		resultArray.push(wordResult)
+	})
+	return resultArray
+}
+
+function findTotal(resultArray){
+	return resultArray.reduce((a,b) => a+b)
+}
+
+function deduce(total){
+	return Array.from(String(total), Number).reduce((a,b) => a+b)
+}
+
+function deducePythagora(pythagoraResults){
+	return pythagoraResults.map(x => { 
+		var r = deduce(x)
+		if(r>9){
+			r = deduce(r)
+		}
+		return r	
+	})
+}
+
+function deduceRecurse(total){
+	var r = Array.from(String(total), Number).reduce((a,b) => a+b)
+	if(r>9){
+		return deduceRecurse(r)
+	} else {
+		var last = r
+		return last
+	}
+	return
+}
